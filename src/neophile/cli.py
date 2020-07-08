0dc38dc806f02727350d3cbe7ef42b7d7fef8be1
Russ Allbery <rra@lsst.org>
2020-07-07 18:22:44 -0700

    Fix help <subcommand> usage information
    Fix the Usage summary when running help <subcommand> to show the
    correct subcommand instead of "help".

--- src/neophile/cli.py
+++ src/neophile/cli.py
@@ -60,6 +60,7 @@ def help(ctx: click.Context, topic: Optional[str]) -> None:
     # https://www.burgundywall.com/post/having-click-help-subcommand
     if topic:
         if topic in main.commands:
+            ctx.info_name = topic
             click.echo(main.commands[topic].get_help(ctx))
         else:
             raise click.UsageError(f"Unknown help topic {topic}", ctx)
